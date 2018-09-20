94322830bfbccc78210d6d69a87ce30abdeca062
jahnava <jahnavalmt@gmail.com>
2018-09-20 13:22:32 -0400

    set up left and right keys to rotate left and right

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 // loads every 30 FPS
 const FPS = 30; //frames per second
+const FRICTION = 0.7;
 const SHIP_SIZE = 30; //ship height set in pixels
+const SHIP_THRUST = 5;
 const TURN_SPEED = 360; //turn speed in degrees per second
 
 // @type {HTMLCanvasElement}
@@ -10,43 +12,48 @@ var ctx = canv.getContext('2d');
 // setting the ship dimentions according to the canvas size
 var ship = {
   x: canv.width / 2, //canvas width divided by two
-  y: canv.height /2, //canvas height divided by two
-  r: SHIP_SIZE /2, //radius- ship size divided by two
-  a: 90 / 180 * Math.PI // angle-convert to radians- if i change the number it changes the direction of the ship
-  rot: 0 //rotation
+  y: canv.height / 2, //canvas height divided by two
+  r: SHIP_SIZE / 2, //radius- ship size divided by two
+  a: 90 / 180 * Math.PI, // angle-convert to radians- if i change the number it changes the direction of the ship
+  rot: 0, //rotation
+  thrusting: false,
+  thrust: {
+    x: 0,
+    y: 0
+  }
 }
 
 //set up event handlers
-document.addEventListener('keydown',keyDown); //event listener with the type keydown and the function called keyDown
-document.addEventListener('keyup',keyUp); //event listener keyup with function keyUp
+document.addEventListener('keydown', keyDown); //event listener with the type keydown and the function called keyDown
+document.addEventListener('keyup', keyUp); //event listener keyup with function keyUp
 
 // set up the game loop
 setInterval(update, 1000 / FPS);
 //setting computer key codes
-function keyDown(//**@type {KeyboardEvent}*/
- ev) {
+function keyDown(ev) {
   switch(ev.keyCode) {
     case 37: //left arrow (rotates ship left)
         ship.rot = TURN_SPEED / 180 * Math.PI / FPS; //rotation speed set to frames per second
       break;
     case 38: //up arrow (thrusts ship forward)
-
+        ship.thrusting = true;
       break;
     case 39: //left arrow (rotates ship right)
         ship.rot = -TURN_SPEED / 180 * Math.PI / FPS; //rotation speed set FPS in the negitive
       break;
   }
+}
 
-  function keyUp(){
+  function keyUp(ev){
     switch(ev.keyCode) {
       case 37: //left arrow (stop ship left rotation)
           ship.rot =  0; //no rotation
         break;
       case 38: //up arrow (stop thrust)
-
+          ship.thrusting = false;
         break;
       case 39: //left arrow (stops ship rotation right)
-          ship.rot = -TURN_SPEED / 180 * Math.PI / FPS; //rotation speed set FPS in the negitive
+          ship.rot = -0; //rotation speed set FPS in the negitive
         break;
   }
 }
